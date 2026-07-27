9c296a35a5176d124d5710ad173ab93b8cbcafc9
studioerse-sudo <studioerse@gmail.com>
2026-07-28 02:05:48 +0530

    Update Script.js

---
+++ Script.js
@@ -0,0 +1,70 @@
+const yes = document.getElementById("yes");
+const no = document.getElementById("no");
+
+
+no.onmouseover = function () {
+    no.style.position = "absolute";
+    no.style.left = Math.random() * 80 + "%";
+    no.style.top = Math.random() * 80 + "%";
+};
+
+yes.onclick = function () {
+
+    document.body.innerHTML = `
+    <div style="text-align:center;margin-top:100px;font-family:Arial;">
+        <h1>❤️ Thank You ❤️</h1>
+
+        <h2>You Made Me So Happy 🥰</h2>
+
+        <img src="bear.gif" width="200">
+
+    </div>
+    `;
+
+}
+
+no.onclick = function () {
+    const x = Math.random() * 250 - 125;
+    const y = Math.random() * 250 - 125;
+
+    no.style.transform = `translate(${x}px, ${y}px)`;
+};setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    document.getElementById("hearts").appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+
+}, 300);
+const create = document.getElementById("create");
+
+create.onclick = function () {
+
+    const name = document.getElementById("name").value;
+    const msg = document.getElementById("message").value;
+
+    const data = btoa(name + "|" + msg);
+
+    const link = location.origin + location.pathname + "#p=" + data;
+
+    prompt("Copy this link", link);
+
+};
+
+if(location.hash.startsWith("#p=")){
+
+    const data = atob(location.hash.replace("#p=",""));
+
+    const arr = data.split("|");
+
+    document.querySelector("h1").innerHTML = "❤️ " + arr[0];
+
+    document.getElementById("showMessage").innerHTML = arr[1];
+
+}
