3e39e920b1212bfcebc2c8ea24c753105a452158
studioerse-sudo <studioerse@gmail.com>
2026-07-28 02:09:17 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,35 @@
 const yes = document.getElementById("yes");
+
+yes.onclick = function () {
+
+document.body.innerHTML = `
+<div style="
+text-align:center;
+margin-top:100px;
+font-family:Arial;
+background:#ffe6f0;
+height:100vh;
+padding-top:50px;
+">
+
+<h1 style="color:#ff0066;">
+❤️ Thank You ❤️
+</h1>
+
+<h2>
+You made me happy 🥰
+</h2>
+
+<div style="font-size:60px;">
+🎉💖🎊
+</div>
+
+<img src="bear.gif" width="220">
+
+</div>
+`;
+
+};
 const no = document.getElementById("no");
 
 
